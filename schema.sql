dfe8252c3fa6437f39e2931803fadf4693239360
Rohan Kumar <therohankumar7@gmail.com>
2021-04-01 20:56:55 -0500

    initial schema example

---
+++ schema.sql
@@ -0,0 +1,25 @@
+CREATE TABLE Users (
+    UserId INT64 NOT NULL,
+    UserName STRING(128) NOT NULL,
+    FirstName STRING(1024) NOT NULL,
+    LastName STRING(1024) NOT NULL,
+    BirthDate DATE,
+    Bio STRING(MAX),
+) PRIMARY KEY (UserId);
+
+CREATE TABLE Tweets (
+    TweetId INT64 NOT NULL,
+    UserId INT64 NOT NULL,
+    Contents STRING(140) NOT NULL,
+    Timestamp TIMESTAMP NOT NULL OPTIONS (allow_commit_timestamp = true),
+    CONSTRAINT FK_UserId FOREIGN KEY (UserId) REFERENCES Users (UserId)
+) PRIMARY KEY (TweetId);
+
+CREATE INDEX TweetsByUserId ON Tweets(UserId);
+
+CREATE TABLE Followers (
+    FollowerId INT64 NOT NULL,
+    FollowingId INT64 NOT NULL,
+    CONSTRAINT FK_FollowerId FOREIGN KEY (FollowerId) REFERENCES Users (UserId),
+    CONSTRAINT FK_FollowingId FOREIGN KEY (FollowingId) REFERENCES Users (UserId),
+) PRIMARY KEY (FollowerId, FollowingId);
